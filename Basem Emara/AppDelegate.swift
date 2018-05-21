60d2dd95d0a1c2739a1daa16e3da3c4b0222b255
Basem Emara <contact@basememara.com>
2018-05-21 09:51:46 -0400

    Initial commit of Clean Architecture rewrite

--- Basem Emara/AppDelegate.swift
+++ Basem Emara/AppDelegate.swift
@@ -2,46 +2,45 @@
 //  AppDelegate.swift
 //  Basem Emara
 //
-//  Created by Basem Emara on 5/13/16.
-//  Copyright © 2016 Zamzam Inc. All rights reserved.
+//  Created by Basem Emara on 2018-05-20.
+//  Copyright © 2018 Zamzam Inc. All rights reserved.
 //
 
 import UIKit
-import UserNotifications
-import SwiftyPress
-import Fabric
-import Crashlytics
-import Alamofire
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, AppPressable {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var urlSessionManager: SessionManager?
 
-    override init() {
-        super.init()
-        AppGlobal.userDefaults.registerSite()
-    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Fabric.with([Crashlytics.self])
-        return didFinishLaunchingSite(application, launchOptions: launchOptions)
+        // Override point for customization after application launch.
+        return true
     }
-    
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
-        return continueUserActivity(application, userActivity: userActivity, restorationHandler: restorationHandler)
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-    
-    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        performFetch(application, completionHandler: completionHandler)
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        didReceiveUserNotification(response: response, withCompletionHandler: completionHandler)
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-    
-    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
-        performActionForShortcutItem(application, shortcutItem: shortcutItem, completionHandler: completionHandler)
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
 }
+
---
+++ Post Notification Extension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  Post Notification Extension
+//
+//  Created by Basem Emara on 2018-05-20.
+//  Copyright © 2018 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
