784f822e8d10d4b995b83d1cb2d4db0b4fd44618
Basem Emara <contact@basememara.com>
2018-06-20 06:20:41 -0400

    Add UIFont extension file under styles

---
+++ Basem Emara/Styles/UIFont.swift
@@ -0,0 +1,13 @@
+//
+//  UIFont.swift
+//  Basem Emara
+//
+//  Created by Basem Emara on 2018-06-20.
+//  Copyright © 2018 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+}
