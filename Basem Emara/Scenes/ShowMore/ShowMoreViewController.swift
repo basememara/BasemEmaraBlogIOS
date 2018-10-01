c9fb4f039e7c50335eb9ebb62df499f9c4b0a7f7
Basem Emara <contact@basememara.com>
2018-09-30 21:16:08 -0400

    Add theme switcher for testing

--- Basem Emara/Scenes/ShowMore/ShowMoreViewController.swift
+++ Basem Emara/Scenes/ShowMore/ShowMoreViewController.swift
@@ -10,4 +10,8 @@ import UIKit
 
 class ShowMoreViewController: UIViewController {
     
+    @IBAction func themeSwitchChanged(_ sender: UISwitch) {
+        let theme: Theme = sender.isOn ? DarkTheme() : LightTheme()
+        theme.apply(for: UIApplication.shared)
+    }
 }
