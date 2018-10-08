5b2cf116143cff64a752f96ac2178bdfe6ec79e4
Basem Emara <contact@basememara.com>
2018-10-07 22:19:45 -0400

    Add another post table view cell template

---
+++ Basem Emara/Common/Controls/PostViewCells/SimplePostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SimplePostTableViewCell.swift
+//  Basem Emara
+//
+//  Created by Basem Emara on 2018-10-07.
+//  Copyright © 2018 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+
+class SimplePostTableViewCell: UITableViewCell, PostsDataViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var featuredImage: UIImageView!
+}
+
+extension SimplePostTableViewCell {
+    
+    func bind(_ model: PostsDataViewModel) {
+        titleLabel.text = model.title
+        featuredImage.setURL(model.imageURL)
+    }
+}
