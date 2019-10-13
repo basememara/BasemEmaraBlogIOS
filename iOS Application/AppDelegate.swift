7ba4b299ee3801006449ea0b6471bb7c64b4dc8d
Basem Emara <contact@basememara.com>
2019-10-12 21:28:12 -0400

    Adjust AppDelegate plugins API

--- iOS Application/AppDelegate.swift
+++ iOS Application/AppDelegate.swift
@@ -13,7 +13,7 @@ import ZamzamCore
 @UIApplicationMain
 class AppDelegate: ApplicationPluggableDelegate {
 
-    private(set) lazy var plugins: [ApplicationPlugin] = [
+    override func application() -> [ApplicationPlugin] {[
         DependencyPlugin(),
         LoggerPlugin(),
         DataPlugin(),
@@ -22,11 +22,14 @@ class AppDelegate: ApplicationPluggableDelegate {
         ShortcutPlugin(),
         NotificationPlugin(),
         ThemePlugin()
-    ]
+    ]}
+}
 
-    override init() {
-        super.init()
-        install(plugins)
+extension AppDelegate {
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
@@ -44,23 +47,5 @@ extension AppDelegate {
     
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         DeepLinkPlugin().application(application, continue: userActivity, restorationHandler: restorationHandler)
-    }
-}
-
-@available(iOS 13.0, *)
-extension AppDelegate {
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
